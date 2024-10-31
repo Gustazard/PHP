49853d90050a1e08a6bb418ba857309b70099782
Gustavo Xavier <gxavier8477@gmail.com>
2024-10-30 21:38:45 -0300

    PHP
    Aprendendo PHP

---
+++ Aulas PHP/aula1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Testando Código em php</h1>
+
+    <?php
+    $nome = "Gustavo Xavier";
+    $idade = 20;
+    $cidade = "São Paulo";
+
+    echo "Eu me chamo $nome e tenho $idade anos, moro em $cidade";
+
+    ?>
+</body>
+
+</html>
